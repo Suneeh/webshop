8c8a0f8639b94afb0d8840c0a42b4d9bf0759a54
Suneeh <suneeh1338@gmail.com>
2025-07-16 13:42:09 +0200

    Add Animation and forgotten migration

---
+++ backend/src/Webshop.Infrastructure/Migrations/20250715102305_AddDiscount.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Webshop.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDiscount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Discounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    ValidUntil = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    Value = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Discounts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DiscountProduct",
+                columns: table => new
+                {
+                    DiscountsId = table.Column<int>(type: "integer", nullable: false),
+                    ProductsId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DiscountProduct", x => new { x.DiscountsId, x.ProductsId });
+                    table.ForeignKey(
+                        name: "FK_DiscountProduct_Discounts_DiscountsId",
+                        column: x => x.DiscountsId,
+                        principalTable: "Discounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DiscountProduct_Products_ProductsId",
+                        column: x => x.ProductsId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DiscountProduct_ProductsId",
+                table: "DiscountProduct",
+                column: "ProductsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DiscountProduct");
+
+            migrationBuilder.DropTable(
+                name: "Discounts");
+        }
+    }
+}
--- frontend/src/app/product/product-list/product-list.component.ts
+++ frontend/src/app/product/product-list/product-list.component.ts
@@ -6,12 +6,23 @@ import { RouterLink } from '@angular/router';
 import { GetProductListDto } from '../../services/api.service.ts/dtos/products/get-product-list-dto';
 import { ProductPriceComponent } from '../product-price/product-price.component';
 import { ProductRatingComponent } from '../product-rating/product-rating.component';
+import { animate, query, stagger, style, transition, trigger } from '@angular/animations';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-product-list',
   imports: [CommonModule, MatCardModule, MatButton, RouterLink, ProductRatingComponent, ProductPriceComponent],
   templateUrl: './product-list.component.html',
+  animations: [
+    trigger('animation', [
+      transition('* <=> *', [
+        query('mat-card', [
+          style({ opacity: 0, transform: 'scale(0.7)' }),
+          stagger(100, [animate('300ms ease-in', style({ opacity: 1, transform: 'scale(1)' }))]),
+        ]),
+      ]),
+    ]),
+  ],
 })
 export class ProductListComponent {
   products = input.required<GetProductListDto[]>();
